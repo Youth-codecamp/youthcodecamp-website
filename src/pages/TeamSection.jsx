d910ce06e1b175217633243e4bf68f2925999a91
Shyaka Chaste <liochastej@gmail.com>
2024-12-27 01:34:46 +0200

    updated

--- src/pages/TeamSection.jsx
+++ src/pages/TeamSection.jsx
@@ -28,8 +28,8 @@ const teamMembers = [
 
   {
     name: 'Ndizeye David',
-    role: 'Frontend developer',
-    linkedin: 'https://www.linkedin.com/in/',
+    role: 'Backend developer',
+    linkedin: 'https://www.linkedin.com/in/david-me-960844343/',
     img: image4,
   },
 
