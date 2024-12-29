a0ee475f4cd33bbaaa503a86ddbe9bcd583b19b7
Shyaka Chaste <liochastej@gmail.com>
2024-12-29 10:32:45 +0200

    updated

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -9,7 +9,7 @@ const Community = () => {
         <p>
           Apply to join our growing community at Youth Code Camp! Here, you can connect with other students, get help with coding, share your projects, and stay updated on our latest events. It’s a friendly place where you can learn, collaborate, and make new friends.
         </p>
-        <a href="https://ed0azuo2uuh.typeform.com/to/LqAQgAdC" className="join-button" target="_blank" rel="noopener noreferrer">Join Our Community</a>
+        <a href="/community" className="join-button">Join Our Community</a>
       </header>
     </div>
   );
